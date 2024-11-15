bc67e66cff0179349cf666665b872840462526de
Mohanapriya4 <73380833+Mohanapriya4@users.noreply.github.com>
2024-11-15 22:48:30 +0530

    React

---
+++ React/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
+
+function App() {
+  
+  return (
+    <div>
+     <Header />
+     <Content />
+     <Footer />
+     
+    </div>
+  );
+}
+
+export default App;
