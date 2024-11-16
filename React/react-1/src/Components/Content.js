04b9f3b98eb6ba2d155318f2ccf2f04ed501291b
Mohanapriya4 <73380833+Mohanapriya4@users.noreply.github.com>
2024-11-16 23:53:31 +0530

    useState

--- React/react-1/src/Components/Content.js
+++ React/react-1/src/Components/Content.js
@@ -1,25 +1,62 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Content = () => {
-    function handleNameChange(){
-        const names = ["Earn", "Grow", "Give"]
-        const int = Math.floor(Math.random()*3)
-        return names[int]
-      }
-      const handleClick=(name)=>{
-       console.log(`Thanks for subscribing ${name}`);
-      }
-
-      const handleClick1=(e)=>{
-        console.log(e.target.innerHTML);
-        
-      }
+  const[name,setName]=useState("Earn")
+  function handleNameChange(){
+    const nameContent = ["Earn","Grow", "Give"]
+    const int = Math.floor(Math.random()*3)
+    setName(nameContent[int])
+  }
   return (
       <main>
-         <p onDoubleClick={()=>handleClick('Priya')}>Lets {handleNameChange()} money</p>
-         <button onClick={(e)=>handleClick1(e)}>Subscribe</button>
+        <p>Let's {name} Money</p>
+        <button onClick={handleNameChange}>Subscribe</button>      
       </main>
   )
 }
-
 export default Content
+//-----------------------------------------------------------------------------------------
+//click events in react:
+// import React from 'react'
+// import { useState } from 'react'
+// const Content = () => {
+//   function handleNameChange(){
+//       const names = ["Earn", "Grow", "Give"]
+//       const int = Math.floor(Math.random()*3)
+//       return names[int]
+//     }
+//     const handleClick=(name)=>{
+//      console.log(`Thanks for subscribing ${name}`);
+//     }
+//     const handleClick1=(e)=>{
+//       console.log(e.target.innerHTML);     
+//     }
+// return (
+//     <main>
+//        <p onDoubleClick={()=>handleClick('Priya')}>Lets {handleNameChange()} money</p>
+//        <button onClick={(e)=>handleClick1(e)}>Subscribe</button>
+//     </main>
+// )
+// }
+// export default Content
+//-----------------------------------------------------------------------------------------
+//UseState:
+// import React from 'react'
+// import { useState } from 'react'
+// const Content = () => {
+//   const [count,setCount]= useState(99)
+//     function handleIncrement(){
+//       setCount((count)=>count+1)
+//     }
+//     function handleDecrement(){
+//       setCount((count)=>count-1)
+//     }
+//   return (
+//       <main>
+//          <button onClick={handleIncrement}>+</button>
+//          <p>{count}</p>
+//          <button onClick={handleDecrement}>-</button>
+//       </main>
+//   )
+// }
+// export default Content
