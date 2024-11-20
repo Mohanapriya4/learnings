b04353696730cf8231351b0ab93815795d0d4892
Mohanapriya4 <73380833+Mohanapriya4@users.noreply.github.com>
2024-11-20 18:13:21 +0530

    Todo-List

--- React/react-1/src/App.js
+++ React/react-1/src/App.js
@@ -4,10 +4,11 @@ import Footer from './Components/Footer';
 import { useState,useEffect } from 'react'
 import Additem from './Components/Additem';
 import Searchitem from './Components/Searchitem';
+import apiRequest from './Components/Apirequest';
 
 
 function App() {
-  const API_URL = 'http://localhost:3500/items'
+  const API_URL = 'http://localhost:3502/items'
   const [items,setItems]=useState([])
 
   const [newItem,setNewItem]=useState('')
@@ -40,27 +41,57 @@ function App() {
     
   },[])
 
-  const addNewItem =(item)=>{
-    const id = items.length?items[items.length-1].id + 1 : 1;
+  const addNewItem = async(item)=>{
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
     const addItem = {id,checked:false,item}
     const ListAddItem = [...items,addItem]
     setItems(ListAddItem)
     // localStorage.setItem("Todo_List",JSON.stringify(ListAddItem))
+
+    const postOption = {
+      method : 'POST',
+      headers : {
+        'Content-Type':'application/json'
+      },
+      body : JSON.stringify(addItem)
+    }
+
+    const result = await apiRequest(API_URL,postOption)
+    if(result) setFetchError(result)
   }
 
-  const handleCheckbox=(id)=>{
+  const handleCheckbox= async(id)=>{
+    
     const newItemsArray = items.map((itemsparam)=>
     itemsparam.id===id?
     {...itemsparam,checked:!itemsparam.checked}:itemsparam)
     setItems(newItemsArray)
     console.log(id);
     // localStorage.setItem("Todo_List",JSON.stringify(newItemsArray))
+
+    const myItem = newItemsArray.filter((item)=>item.id===id)
+    const updateOption = {
+      method : 'PATCH',
+      headers : {
+        'Content-Type':'application/json'
+      },
+      body : JSON.stringify({checked:myItem[0].checked}) 
+    }
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl,updateOption)
+    if(result) setFetchError(result)
   }
 
-  const handleDelete=(id)=>{
+  const handleDelete= async (id)=>{
     const newDeleteArray = items.filter((itemsparam1)=>(itemsparam1.id!==id))
     setItems(newDeleteArray)
     // localStorage.setItem("Todo_List",JSON.stringify(newDeleteArray))
+    const deleteOption = {method:'DELETE'}
+   
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl,deleteOption)
+    if(result) setFetchError(result)
+  
   }
 
   const handleDoubleTick=(id)=>{
@@ -80,7 +111,7 @@ function App() {
   
   return (
     <div className="App">
-     <Header title="Mohanapriya TODO"/>
+     <Header title="TODO LIST"/>
      <Additem 
      newItem={newItem}
      setNewItem={setNewItem}
